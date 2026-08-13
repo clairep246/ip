df182e705d83daa8959debad404239f1feecb324
claire p <clairephay@gmail.com>
2026-08-13 12:46:23 +0800

    Added echo command

--- src/main/java/Bags.java
+++ src/main/java/Bags.java
@@ -1,12 +1,6 @@
-
-/**
- * Starts the Bags chatbot and displays its initial greeting.
- */
+import java.util.Scanner;
 public class Bags {
-    /** Separates sections of the chatbot's output. */
     private static final String divider = "____________________________________________________________";
-
-    /** The banner displayed when the chatbot starts. */
     private static final String banner = " ____                  \n"
             + "| __ )  __ _  __ _ ___ \n"
             + "|  _ \\ / _` |/ _` / __|\n"
@@ -15,11 +9,38 @@ public class Bags {
             + "               |___/";
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Printing
         System.out.println(divider);
         System.out.println(banner);
         System.out.println("Hello! I'm Bags. Nice to meet you!");
         System.out.println("What can I do for you?");
-        System.out.println(divider);
-        System.out.println("Bye. Hope to see you again soon!");
+
+        String output = scanner.nextLine();
+
+        if (output.equals("echo")) {
+            System.out.println("From now on I will echo your input, please enter your input. To exit enter bye");
+
+            String echo = scanner.nextLine();
+
+            while (!echo.equals("bye")) {
+                System.out.println(echo);
+                echo = scanner.nextLine();
+            }
+            System.out.println(divider);
+            System.out.println("Bye. Hope to see you again soon!");
+
+        } else if (output.equals("bye")) {
+            System.out.println(divider);
+            System.out.println("Bye. Hope to see you again soon!");
+
+        } else {
+            System.out.println("I don't know that command.");
+            System.out.println(divider);
+            System.out.println("Bye. Hope to see you again soon!");
+        }
+
+        scanner.close();
     }
 }
