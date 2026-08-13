97bf392efdde4bc3aacdf13448c93bf5c7f1b3cb
claire p <clairephay@gmail.com>
2026-08-13 12:55:34 +0800

    Improved edit mode. End of Level 1

--- src/main/java/Bags.java
+++ src/main/java/Bags.java
@@ -14,31 +14,40 @@ public class Bags {
         // Printing
         System.out.println(divider);
         System.out.println(banner);
+        System.out.println(divider);
         System.out.println("Hello! I'm Bags. Nice to meet you!");
+        System.out.println(divider);
         System.out.println("What can I do for you?");
+        System.out.println(divider);
 
         String output = scanner.nextLine();
-
+        System.out.println(divider);
         if (output.equals("echo")) {
             System.out.println("From now on I will echo your input, please enter your input. To exit enter bye");
+            System.out.println(divider);
 
             String echo = scanner.nextLine();
+            System.out.println(divider);
 
             while (!echo.equals("bye")) {
                 System.out.println(echo);
                 echo = scanner.nextLine();
+                System.out.println(divider);
             }
             System.out.println(divider);
             System.out.println("Bye. Hope to see you again soon!");
+            System.out.println(divider);
 
         } else if (output.equals("bye")) {
             System.out.println(divider);
             System.out.println("Bye. Hope to see you again soon!");
+            System.out.println(divider);
 
         } else {
             System.out.println("I don't know that command.");
             System.out.println(divider);
             System.out.println("Bye. Hope to see you again soon!");
+            System.out.println(divider);
         }
 
         scanner.close();
