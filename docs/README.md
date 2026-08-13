85ee094c502202e27e375b308aeec61624d0e9ea
claire p <clairephay@gmail.com>
2026-08-13 12:15:31 +0800

    Changed name of chatbot

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Bags User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Bags.java
@@ -0,0 +1,12 @@
+
+public class Bags {
+    public static void main(String[] args) {
+        String banner = " ____                  \n"
+                + "| __ )  __ _  __ _ ___ \n"
+                + "|  _ \\ / _` |/ _` / __|\n"
+                + "| |_) | (_| | (_| \\__ \\\n"
+                + "|____/ \\__,_|\\__, |___/\n"
+                + "               |___/    \n";
+        System.out.println(banner);
+    }
+}
